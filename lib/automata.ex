45b8e7590e5d75768c94e0c038b566e524abc325
MiguelMonroy <A01739891@tec.mx>
2026-04-17 19:00:09 -0600

    Test y codigo para el primer ejercicio

--- lib/automata.ex
+++ lib/automata.ex
@@ -1,18 +1,65 @@
 defmodule Automata do
-  @moduledoc """
-  Documentation for `Automata`.
-  """
 
-  @doc """
-  Hello world.
+  defstruct states: [],
+            alphabet: [],
+            transitions: %{},
+            start: nil,
+            accept: []
 
-  ## Examples
 
-      iex> Automata.hello()
-      :world
+  def move(automaton, states, symbol) do
+    states
+    |> Enum.flat_map(fn state ->
+      Map.get(automaton.transitions, {state, symbol}, [])
+    end)
+    |> Enum.uniq()
+  end
 
-  """
-  def hello do
-    :world
+
+  def determinize(nfa) do
+    start = [nfa.start]
+
+    process([start], [], %{}, nfa)
+  end
+
+  defp process([], states, transitions, nfa) do
+    %Automata{
+      states: states,
+      alphabet: nfa.alphabet,
+      transitions: transitions,
+      start: [nfa.start],
+      accept:
+        Enum.filter(states, fn state_set ->
+          Enum.any?(state_set, fn s -> s in nfa.accept end)
+        end)
+    }
+  end
+
+  defp process([current | rest], states, transitions, nfa) do
+
+    states =
+      if current in states do
+        states
+      else
+        [current | states]
+      end
+
+    {new_transitions, new_queue} =
+      Enum.reduce(nfa.alphabet, {transitions, rest}, fn symbol, {t_acc, q_acc} ->
+        next = move(nfa, current, symbol)
+
+        t_acc = Map.put(t_acc, {current, symbol}, next)
+
+        q_acc =
+          if next == [] or next in states or next in q_acc do
+            q_acc
+          else
+            [next | q_acc]
+          end
+
+        {t_acc, q_acc}
+      end)
+
+    process(new_queue, states, new_transitions, nfa)
   end
 end
--- test/automata_test.exs
+++ test/automata_test.exs
@@ -2,7 +2,26 @@ defmodule AutomataTest do
   use ExUnit.Case
   doctest Automata
 
-  test "greets the world" do
-    assert Automata.hello() == :world
+  test "determinize convierte NFA a DFA correctamente" do
+    nfa = %Automata{
+      states: [0, 1],
+      alphabet: [:a],
+      transitions: %{
+        {0, :a} => [0, 1]
+      },
+      start: 0,
+      accept: [1]
+    }
+
+    dfa = Automata.determinize(nfa)
+
+    assert dfa.start == [0]
+
+    assert Enum.sort(dfa.states) == Enum.sort([[0], [0, 1]])
+
+    assert dfa.transitions[{[0], :a}] == [0, 1]
+    assert dfa.transitions[{[0, 1], :a}] == [0, 1]
+
+    assert dfa.accept == [[0, 1]]
   end
 end
